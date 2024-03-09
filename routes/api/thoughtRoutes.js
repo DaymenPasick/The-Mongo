8f9291b330cfdbd33f8093f2b3ca586dc4e21041
DaymensCode <Daymenscode@gmail.com>
2024-03-09 00:46:08 -0500

    finished creating basic setup for thoughtRoutes.js that will point to its controller file when I create it

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+
+//this const group points to our thought route methods
+//within the controller directory
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+//all of these are defined in controllers
+  .route('/:ThoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:videoId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
