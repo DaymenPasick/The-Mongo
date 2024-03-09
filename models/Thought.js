54b3766028542a70b1805208df299463c1627e9a
DaymensCode <Daymenscode@gmail.com>
2024-03-08 23:32:09 -0500

    copied User model into Thought model as a template

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,50 @@
 //this will allow us to use mongoos's Schema class and model
 const { Schema, model } = require('mongoose');
+
+
+// Schema for User model
+const userSchema = new Schema(
+    {
+      //formatting for username
+      username: {
+        type: String,
+        required:true,
+        trim:true,
+      },
+
+      //formatting for email
+      email: {
+        type: String,
+        required:true,
+        //this will make sure no emails are the same
+        unique:true,
+        //needs validation for matching email adress format
+      },
+
+      //creates association with Thought model
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+
+      //creates self-association with User model
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'user',
+        },
+      ],
+    },
+    {
+      //added this in case we do want to include virtuals, but may not need
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+  
+  //need to do email validation
+  //need to add virtual of friendCount that retrieves length of the user's friends array
