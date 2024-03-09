9e602afab93e8211746b1f7edb1a91b34840493b
DaymensCode <Daymenscode@gmail.com>
2024-03-09 01:12:25 -0500

    switched missplaced userController file data to the correct file xD woops haha

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+//this will will contain the logic/methods behind
+//our user api routes
+const User = require('../models/User');
+
+module.exports = {
+  //find all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //find a single use
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  //create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+
+
+
+
+//need to method for add put and delete by id routes to 
+//update and delete users by their id
+
+//need post method to add new friend to user friend list
+//need delete method to remove friend for users friends list
+
+};
