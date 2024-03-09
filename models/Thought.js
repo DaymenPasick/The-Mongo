3ef790821825907db6b3785b9dd8621fc174475c
DaymensCode <Daymenscode@gmail.com>
2024-03-09 00:22:33 -0500

    finished basic setup of reactionSchema within Thought model. May need some reworking"

--- models/Thought.js
+++ models/Thought.js
@@ -4,9 +4,35 @@ const { Schema, model } = require('mongoose');
 
 //for reference to use in thoughtSchema
 const reactionSchema = new Schema (
+    {
+    //will give each reaction an objectId when created
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
 
 
+    reactionBody: {
+        type: String,
+        required:true,
+        //will set length max of 1~280 chars
+        maxLength:280,
 
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+    
+      //formatting for timestamp of creation
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        //need getter method for formatting upon query
+      },
+
+ }
 )
 
 
@@ -39,7 +65,7 @@ const thoughtSchema = new Schema(
         },
 
 
-      reaction : []
+      reaction : [reactionSchema]
 
 
 
