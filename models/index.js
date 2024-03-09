0259f6bade30ec394102b5909b1b1de6dac162f0
DaymensCode <Daymenscode@gmail.com>
2024-03-08 23:05:41 -0500

    changed thoughts to thought in index.js of model directory

--- models/index.js
+++ models/index.js
@@ -2,9 +2,9 @@
 //neatly in one export file
 const User = require('./User');
 const Reaction = require('./Reaction'); //may not need this per challenge reqs
-const Thoughts = require('./Thoughts');
+const Thought = require('./Thoughts');
 
 
 //this will give us the ability to call upon our models
 //elsewhere in our code
-module.exports = { User, Thoughts, Reaction };
+module.exports = { User, Thought, Reaction };
