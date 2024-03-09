f37f55cf104bf0ae2a2fcad1733ca48a74c8e627
DaymensCode <Daymenscode@gmail.com>
2024-03-08 23:55:58 -0500

    deleted Reaction model.js and references in index.js within model directory

--- models/Reaction.js
+++
@@ -1,2 +0,0 @@
-//this will allow us to use mongoos's Schema class and model
-const { Schema, model } = require('mongoose');
--- models/index.js
+++ models/index.js
@@ -1,10 +1,9 @@
 //this file will help us keep our model references grouped
 //neatly in one export file
 const User = require('./User');
-const Reaction = require('./Reaction'); //may not need this per challenge reqs
 const Thought = require('./Thoughts');
 
 
 //this will give us the ability to call upon our models
 //elsewhere in our code
-module.exports = { User, Thought, Reaction };
+module.exports = { User, Thought };
