e089e961409a7c7950279dcfbe1692ce41038373
DaymensCode <Daymenscode@gmail.com>
2024-03-09 01:07:08 -0500

    made controller directory and thought/userController.js files

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -28,6 +28,6 @@ router
 router.route('/:thoughtId/reactions').post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:videoId/reactions/:reactionId').delete(removeReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
