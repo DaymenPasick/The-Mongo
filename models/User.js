27bc7512064f0395476df4d6160e324ba0c5f79f
DaymensCode <Daymenscode@gmail.com>
2024-03-08 23:14:52 -0500

    created initial user schema for our User.js model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,34 @@
 //this will allow us to use mongoos's Schema class and model
 const { Schema, model } = require('mongoose');
+
+
+// Schema for User model
+const userSchema = new Schema(
+    {
+      //changed in class
+      first: {
+        type: String,
+        required:true,
+      },
+      //changed in class
+      last: {
+        type: String,
+        required:true,
+      },
+      age: Number,
+      //creating association with thoughts Schema
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'thought',
+        },
+      ],
+    },
+    {
+      //added this in case we do want to include virtuals, but may not need
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
