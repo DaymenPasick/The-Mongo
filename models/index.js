2946591b0a8756cb5f17a493d1a322039ed24b01
DaymensCode <Daymenscode@gmail.com>
2024-03-08 23:04:25 -0500

    updated index.js in models directory to have user, reactions, and thoughts model exports, also added guide comments

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
-const Pokedepartment = require('./Pokedepartment');
+//this file will help us keep our model references grouped
+//neatly in one export file
+const User = require('./User');
+const Reaction = require('./Reaction'); //may not need this per challenge reqs
+const Thoughts = require('./Thoughts');
 
-module.exports = { Pokedepartment };
+
+//this will give us the ability to call upon our models
+//elsewhere in our code
+module.exports = { User, Thoughts, Reaction };
